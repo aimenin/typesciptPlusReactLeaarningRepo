723ab45116fbe5094eaedb9f49fdc2f499436d63
aimen <artem.igorevich8@rambler.ru>
2022-01-20 20:27:06 +0300

    add guest adding handler and typed it

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,13 +2,22 @@ import { useState } from 'react';
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState('');
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const handleGuestAdd = () => {
+    setName('');
+    setGuests([...guests, name]);
+  }
 
   return (
     <div>
       <h3>Guest List</h3>
 
       <input value={name} onChange={(e) => setName(e.target.value)}/>
-      <button>Add guest</button>
+      <button onClick={handleGuestAdd}>Add guest</button>
+      {guests.map((guest) => {
+        return (<p>{guest}</p>)
+      })}
     </div>
   );
 };
