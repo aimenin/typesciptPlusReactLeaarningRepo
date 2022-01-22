82626bbd8b21d11db4201742678070f76a42fd9d
aimen <artem.igorevich8@rambler.ru>
2022-01-22 19:38:28 +0300

    add refs and useEffect work together

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
   {name: 'Sarah', age: 20},
@@ -10,6 +10,13 @@ const UserSearch: React.FC = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string, age: number } | undefined>();
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    } // typeguard
+    inputRef.current.focus()
+  }, [])
 
   const handleUserSearch = () => {
     const foundUser = users.find((user) => {
