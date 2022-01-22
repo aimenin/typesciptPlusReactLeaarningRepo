d2d1570ad0f6f1819051073bd77340d3ffae0d6b
aimen <artem.igorevich8@rambler.ru>
2022-01-22 18:58:44 +0300

    add typed drag event

--- src/events/EventCpmponent.tsx
+++ src/events/EventCpmponent.tsx
@@ -1,10 +1,17 @@
+import React from "react";
+
 const EventComponent: React.FC = () => {
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
   }
 
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log(e.clientX);
+  }
+
   return <div>
     <input onChange={handleOnChange} />
+    <div draggable onDragStart={handleDragStart} style={{ cursor: 'grab' }}>Drag me</div>
   </div>
 }
 
