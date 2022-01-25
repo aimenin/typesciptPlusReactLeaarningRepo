66a5f6fba45d7ce58568963dd67ca4a54014c936
aimen <artem.igorevich8@rambler.ru>
2022-01-25 19:07:31 +0300

    add calling our action creators and add custom hook for it

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
+import { useActions } from "../hooks/useActions";
+
 const RepositoriesList: React.FC = () => {
+  const [search, setSearch] = useState('');
+  const { searchRepositories } = useActions();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    searchRepositories(search);
+  }
+
   return <div>
-    <form>
-      <input />
+    <form onSubmit={handleSubmit}>
+      <input value={search} onChange={(e) => setSearch(e.target.value)}/>
       <button>Search</button>
     </form>
   </div>
